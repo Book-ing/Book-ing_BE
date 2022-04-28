d836070bcf7ddc9849a99066596bde0f2340b1a8
MoingXTwice <sckbs1314@gmail.com>
2022-04-28 23:20:43 +0900

    feat(meeting): add new API for meeting
    모임 방을 생성하기 위한 API 추가
    이미지 업로드를 위한 multer와 유효성 검사를 위한 validator를 middlewares 폴더에 추가하였습니다.
    
    2022.04.28 actual project: book-ing
    commit by MoingXTwice

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 const kakao = require('./auth')
+const meetingRouter = require('./meeting');
 
 router.use('/auth', kakao)
+router.use('/meeting', meetingRouter);
+
 module.exports = router;
