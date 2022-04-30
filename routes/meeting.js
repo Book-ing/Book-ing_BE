ea81f87be3c80b77f4a95b46cda3b3fabfe62281
Hojin <gbolt@naver.com>
2022-04-30 18:38:17 +0900

    feat (study): 스터디 api
    스터디 조회, 참가 등록 api
    
    스터디를 등록: 스터디를 만드는 사람은 만든 즉시 방장이 되고 바로 멤버로
    들어오게 된다. 스터디를 등록할 때 사진을 첨부하지 않으면 기본이미지가
    올라간다.
    스터디 참가, 취소 : 현재는 join,quit TYPE으로 참가와 취소를 결정한다.
    스터디 방장은 스터디를 나갈 수 없고 나머지 멤버들은 자유롭게 참가,취소가
    가능하다.
    
    2022.04.30 실전 프로젝트 Book-ing
    commit by ho-bolt

--- routes/meeting.js
+++ routes/meeting.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const meetingController = require('../controllers/meetingController');
+const studyController = require('../controllers/studyController');
 const upload = require('../middlewares/multer');
 const createMeetingValidation = require('../middlewares/validator')
 
 router.post('/', upload.single('meetingImage'), createMeetingValidation, meetingController.createMeeting);
-
+router.get('/:meetingId/study', studyController.getStudyLists);
 module.exports = router;
--- routes/study.js
+++ routes/study.js
@@ -3,9 +3,12 @@ const studyController = require('../controllers/studyController')
 const createStudyValidation = require('../middlewares/studyValidator')
 const router = express.Router();
 
+//api/study
 
 
 router.post('/', createStudyValidation, studyController.postStudy)
-
+router.put('/', studyController.updateStudy)
+router.get('/:meetingId/study', studyController.getStudyLists)
+router.post('/joinedstudy', studyController.joinStudy)
 
 module.exports = router
