c8f23be9261cf836bcac760e171361c80f9fe815
MoingXTwice <sckbs1314@gmail.com>
2022-05-04 14:44:08 +0900

    fix(meeting): fix router and validator for meeting
    모임 정보 수정 유효성 검사를 추가하였습니다.
    
    2022.05.04 실전프로젝트 Book-ing
    commit by MoingXTwice

--- routes/meeting.js
+++ routes/meeting.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const meetingController = require('../controllers/meetingController');
 const studyController = require('../controllers/studyController');
 const upload = require('../middlewares/multer');
-const createMeetingValidation = require('../middlewares/validator');
+const {
+    createMeetingValidation,
+    modifyMeetingValidation,
+} = require('../middlewares/validator');
 
 router.post(
     '/',
@@ -15,7 +18,7 @@ router.get('/:meetingId', meetingController.getMeetingInfo);
 router.get('/:meetingId/users', meetingController.getMeetingUsers);
 router.post('/inout', meetingController.inoutMeeting);
 router.post('/kickuser', meetingController.kickMeetingMember);
-router.put('/', upload.single('meetingImage'), meetingController.modifyMeeting);
+router.put('/', modifyMeetingValidation, upload.single('meetingImage'), meetingController.modifyMeeting);
 router.delete('/:meetingId', meetingController.deleteMeeting);
 
 module.exports = router;
