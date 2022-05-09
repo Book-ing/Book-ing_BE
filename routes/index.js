3072c21c89560305262ccd0323edb90a312e7833
MoingXTwice <sckbs1314@gmail.com>
2022-05-09 18:10:00 +0900

    feat(chat): add new API for chat
    채팅 조회 API 및 routes 폴더의 index 파일을 수정하였습니다.
    
    2022.05.09 실전프로젝트 Book-ing
    commit by MoingXTwice

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const mainRouter = require('./main');
 const studyRouter = require('./study');
 const meetingRouter = require('./meeting');
 const mypage = require('./mypage');
+const chat = require('./chat');
 
 router.use('/study', studyRouter);
 router.use('/auth', kakao);
 router.use('/main', mainRouter);
 router.use('/meeting', meetingRouter);
 router.use('/mypage', mypage);
+router.use('/chat', chat);
 
 module.exports = router;
