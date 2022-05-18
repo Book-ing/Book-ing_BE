99959dadab28301a2503f1f8b6f26b542b77263f
MoingXTwice <sckbs1314@gmail.com>
2022-05-18 17:56:19 +0900

    fix(chat): fix controller and routes for chat
    chat의 getChat API 를 수정하였습니다.
    
    2022.05.18 실전프로젝트 Book-ing
    commit by MoingXTwice

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const chatController = require('../controllers/chatController');
+const authMiddleware = require('../middlewares/auth-middlewares');
 
-router.get('/:meetingId', chatController.getChat);
+router.get('/:meetingId', authMiddleware, chatController.getChat);
 
 module.exports = router;
