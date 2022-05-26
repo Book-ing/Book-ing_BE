fbd08301c5e47cb97db1884b9345e9561c7eb396
Hojin <gbolt@naver.com>
2022-05-26 15:46:41 +0900

    chore(onOffStudy): socket.js 복원
    소켓.js 다시 복원
    
    2022-05-26 실전프로젝트 Book-ing
    commit by ho-bolt

--- socket.js
+++ socket.js
@@ -22,222 +22,218 @@ const io = require('socket.io')(server, {
     },
 });
 
-//방 배열 (roomId)
+//방 배열 (studyId)
 let roomObjArr = [];
-let mediaStatus = {};
-const MAXIMUM = 10; //TODO 바꿔야됨
-
-const roomNameCreator = (roomId, type) => {
-    type = type === 'room' ? 'study' : 'meeting';
-    return type + roomId;
-};
-
-async function findMember(type, roomId, userId) {
-    let existMember;
-    if (type === 'room') {
-        console.log('스터디 타입을 탔어요');
-        existMember = await STUDYMEMBERS.findOne({
-            studyId: roomId,
-            studyMemberId: userId,
-        });
-    } else if (type === 'crew') {
-        console.log('모임 타입을 탔어요');
-        existMember = await MEETINGMEMBER.findOne({
-            meetingId: roomId,
-            meetingMemberId: userId,
-        });
-    }
-    return existMember;
-}
+let mediaStatus = {}
+const MAXIMUM = 10;
 
 io.on('connection', (socket) => {
-    console.log('connection socketId : ', socket.id);
-    let myRoom = null;
-    // let myRoomName = null;
-    let myNickname = null;
-
-    // roomName===roomId, nickname===userId
-    // type = room(study) or crew(meeting)
-    socket.on('joinRoom', async (roomId, userId, type) => {
-        console.log('roomId : ', roomId, ' userId : ', userId, ' type : ', type);
-        // 공통적인 부분
-        let existMember = await findMember(type, roomId, userId);
-        if (!roomId || !userId || !existMember) return;
-
-        const roomName = roomNameCreator(roomId, type);
-        console.log('roomName : ', roomName);
-
-        const user = await USER.findOne({ userId });
-        const nickname = user.username;
-
-        // Start of if
-        // video
-        if (type === 'room') {
-            console.log('스터디에 들어와서 비디오 부분이 시작돼요');
-            // myRoomName = roomId;
-            myNickname = nickname;
-            myRoom = roomId;
-            console.log('joinRoom', 'roomId :', roomId, 'nickname:', nickname);
-
-            let isRoomExist = false;
-            let targetRoomObj = null;
-
-            //만약
-            if (!mediaStatus[roomId]) {
-                mediaStatus[roomId] = {};
-            }
-
-            for (let i = 0; i < roomObjArr.length; i++) {
-
-                // 스터디 룸
-                if (roomObjArr[i].roomId === roomId) {
-                    // 정원 초과
-                    if (roomObjArr[i].currentNum >= MAXIMUM) {
-                        //정원 초과라 거부 이벤트 실행
-                        socket.emit('rejectJoin');
-                        return;
-                    }
-                    // 스터디 멤버 체크
-                    // const joinedMemeber = await STUDYMEMBERS.findOne({ studyId })
-                    // if (username !== joinedMemeber.studyMemberId) {
-                    //     socket.emit('rejectUnvalidUser')
-                    //     return
-                    // }
-
-                    //정원 초과 아니면 해당 스터디 룸에 참여
-                    isRoomExist = true;
-                    //맞는 룸에 들어감
-                    targetRoomObj = roomObjArr[i];
-                    break;
-                }
-            }
-
-            // 입력한 룸이름이 없다면 새로운 방 만듦
-            // 방이 존재하지 않는다면 방을 생성
-
-            if (!isRoomExist) {
-                targetRoomObj = {
-                    roomId,
-                    currentNum: 0,
-                    users: [],
-                };
-                roomObjArr.push(targetRoomObj);
-            }
-
-            console.log('joinRoom', 'targetRoomObj : ', targetRoomObj);
-            // 어떠한 경우든 방에 참여
-            targetRoomObj.users.push({
-                socketId: socket.id,
-                nickname,
-            });
-            targetRoomObj.currentNum++;
-            // 입력한 방에 입장
-            console.log(
-                `${ nickname }이 방 ${ roomName }에 입장 (${ targetRoomObj.currentNum }/${ MAXIMUM })`,
-            );
-
-            mediaStatus[roomId][socket.id] = {
-                screensaver: false,
-                muted: false,
-            };
-            //방에 참가하는 거 수락  3.
-            //입장할 때 socket.id 같이 보냄
-            socket.emit('joinStudyRoom', targetRoomObj.users, socket.id);
-            socket.emit('checkCurStatus', mediaStatus[roomId]);
-        }
-        // End of if
-
-        const chats = await CHAT.find({ roomId: roomName })
-                                .limit(50)
-                                .sort({ chatId: -1 })
-                                .then(result => result.sort((a, b) => a.chatId - b.chatId));
-        const chatsUserId = chats.map(result => result.userId);
-        const usersProfile = await USER.find({ userId: chatsUserId });
-
-        const Messages = [];
-        for (let i = 0; i < chats.length; i++) {
-            const roomId = chats[i].roomId;
-            const userId = chats[i].userId;
-            const message = chats[i].message;
-            const regDate = chats[i].regDate;
-            const chatId = chats[i].chatId;
-            for (let j = 0; j < usersProfile.length; j++) {
-                const username = usersProfile[j].username;
-                const profileImage = usersProfile[j].profileImage;
-                if (usersProfile[j].userId === userId) {
-                    Messages.push({
-                        roomId,
-                        chatId,
-                        message,
-                        regDate,
-                        userId,
-                        username,
-                        profileImage,
-                    });
-                }
-            }
-        }
-        // 입장
-        socket.join(roomName);
-        io.to(roomName).emit('getMessages', Messages); // db에 저장된 메세지를 보내준다.
+    console.log('socketId : ', socket.id);
+    socket.on('disconnect', () => {
+        console.log('disconnect socketId : ', socket.id);
     });
+    let myRoom = null;
+
+    socket.on('joinMeetingRoom', async (meetingId, userId) => {
+        if (!meetingId || !userId) return;
+        const existMember = await MEETINGMEMBER.findOne({
+            meetingId,
+            meetingMemberId: userId,
+        });
+        if (existMember) {
+            socket.join('meeting', meetingId);
+            const chats = await CHAT.find({ meetingId })
+                .limit(50)
+                .sort({ chatId: -1 })
+                .then(result => result.sort((a, b) => a.chatId - b.chatId));
+            const chatsUserId = chats.map(result => result.userId);
+            const usersProfile = await USER.find({ userId: chatsUserId });
+
+            const Messages = [];
+            for (let i = 0; i < chats.length; i++) {
+                const meetingId = chats[i].meetingId;
+                const userId = chats[i].userId;
+                const message = chats[i].message;
+                const regDate = chats[i].regDate;
+                const chatId = chats[i].chatId;
+                for (let j = 0; j < usersProfile.length; j++) {
+                    const username = usersProfile[j].username;
+                    const profileImage = usersProfile[j].profileImage;
+                    if (usersProfile[j].userId === userId) {
+                        Messages.push({
+                            meetingId,
+                            chatId,
+                            message,
+                            regDate,
+                            userId,
+                            username,
+                            profileImage,
+                        });
+                    }
+                }
+            }
+            io.to('meeting', meetingId).emit('getMessages', Messages); // db에 저장된 메세지를 보내준다.
+            console.log(`${userId} join a ${meetingId} Room`);
+        }
+    });
+
+    let myRoomName = null
+    let myNickname = null
+
+    // roomName===studyId, nickname===userId
+    socket.on('joinRoom', async (studyId, nickname) => {
+        myRoomName = studyId
+        myNickname = nickname
+        myRoom = studyId;
+        console.log('joinRoom', 'studyId :', studyId, 'nickname:', nickname);
+
+        let isRoomExist = false
+        let targetRoomObj = null
+
+        //만약 
+        if (!mediaStatus[studyId]) {
+            mediaStatus[studyId] = {}
+        }
+
+
+        for (let i = 0; i < roomObjArr.length; i++) {
+
+            // 스터디 룸 
+            if (roomObjArr[i].studyId === studyId) {
+                // 정원 초과
+                if (roomObjArr[i].currentNum >= MAXIMUM) {
+                    //정원 초과라 거부 이벤트 실행 
+                    socket.emit('rejectJoin')
+                    return
+                }
+                // 스터디 멤버 체크 
+                // const joinedMemeber = await STUDYMEMBERS.findOne({ studyId })
+                // if (username !== joinedMemeber.studyMemberId) {
+                //     socket.emit('rejectUnvalidUser')
+                //     return
+                // }
+
+                //정원 초과 아니면 해당 스터디 룸에 참여 
+                isRoomExist = true
+                //맞는 룸에 들어감 
+                targetRoomObj = roomObjArr[i]
+                break
+            }
+        }
+
+        // 입력한 룸이름이 없다면 새로운 방 만듦 
+        // 방이 존재하지 않는다면 방을 생성
+
+        if (!isRoomExist) {
+            targetRoomObj = {
+                studyId,
+                currentNum: 0,
+                users: [],
+            }
+            roomObjArr.push(targetRoomObj)
+        }
+
+        console.log('joinRoom', 'targetRoomObj : ', targetRoomObj);
+        // 어떠한 경우든 방에 참여
+        targetRoomObj.users.push({
+            socketId: socket.id,
+            nickname,
+        })
+        targetRoomObj.currentNum++
+        // 입력한 방에 입장 
+        console.log(
+            `${nickname}이 방 ${studyId}에 입장 (${targetRoomObj.currentNum}/${MAXIMUM})`
+        )
+
+        mediaStatus[studyId][socket.id] = {
+            screensaver: false,
+            muted: false,
+        }
+        // 입장 
+        socket.join(studyId)
+        //방에 참가하는 거 수락  3. 
+        //입장할 때 socket.id 같이 보냄 
+        socket.emit('joinStudyRoom', targetRoomObj.users, socket.id)
+        socket.emit('checkCurStatus', mediaStatus[studyId])
+    })
+
 
     socket.on('ice', (ice, remoteSocketId) => {
-        console.log('ice 이벤트', 'ice : ', ice, 'remoteSocketId', remoteSocketId);
-        socket.to(remoteSocketId).emit('ice', ice, socket.id);
-    });
+        console.log('ice 이벤트', 'ice : ', ice, 'remoteSocketId', remoteSocketId)
+        socket.to(remoteSocketId).emit('ice', ice, socket.id)
+    })
 
     socket.on('offer', (offer, remoteSocketId, localNickname) => {
-        console.log('offer 이벤트', 'offer : ', offer, 'remoteSocketId', remoteSocketId, 'localNickname : ', localNickname);
-        socket.to(remoteSocketId).emit('offer', offer, socket.id, localNickname);
-    });
+        console.log('offer 이벤트', 'offer : ', offer, 'remoteSocketId', remoteSocketId, 'localNickname : ', localNickname)
+        socket.to(remoteSocketId).emit('offer', offer, socket.id, localNickname)
+    })
     // 다른 브라우저에서 보낸 answer 받음 =>5. 
     socket.on('answer', (answer, remoteSocketId) => {
         // 받은 answer  
-        console.log('answer 이벤트', 'answer : ', answer, 'remoteSocketId', remoteSocketId);
-        socket.to(remoteSocketId).emit('answer', answer, socket.id);
-    });
+        console.log('answer 이벤트', 'answer : ', answer, 'remoteSocketId', remoteSocketId)
+        socket.to(remoteSocketId).emit('answer', answer, socket.id)
+    })
 
     //방에 나갔을 때 
     socket.on('disconnecting', async () => {
-        socket.to(myRoom).emit('leave_room', socket.id);
+        console.log('disconnecting')
+        socket.to(myRoom).emit('leave_room', socket.id)
         console.log('disconnecting', 'myRoom : ', myRoom, 'socket.id : ', socket.id);
         for (let i = 0; i < roomObjArr.length; i++) {
-            if (roomObjArr[i].roomId === myRoom) {
+            if (roomObjArr[i].studyId === myRoom) {
                 const newUsers = roomObjArr[i].users.filter(
-                    (user) => user.socketId !== socket.id,
-                );
-                roomObjArr[i].users = newUsers;
+                    (user) => user.socketId !== socket.id
+                )
+                roomObjArr[i].users = newUsers
                 roomObjArr[i].currentNum--;
                 break;
             }
         }
-        console.log('룸에 나간 이후', roomObjArr);
+        console.log("룸에 나간 이후", roomObjArr)
+    })
+
+
+
+    socket.on('leaveMeetingRoom', (meetingId, userId) => {
+        socket.leave('meeting', meetingId);
+        console.log(`${userId} leave a ${meetingId} Room`);
     });
 
-    socket.on('chat message', async (roomId, userId, message, type) => { //TODO type 받아야함
-        console.log('roomId: ', roomId, ' userId : ', userId, ' message : ', message, ' type : ', type);
-        let existMember = await findMember(type, roomId, userId);
-        if (!roomId || !userId || !existMember) return;
-
-        const roomName = roomNameCreator(roomId, type);
-        console.log('roomName : ', roomName);
-
-        const userProfile = await USER.findOne({ userId });
-        const chatMessage = {
-            userId,
-            username: userProfile.username,
-            profileImage: userProfile.profileImage,
-            message,
-            regDate: lib.getDate(),
-        };
-        await CHAT.create({
-            roomId: roomName,
-            userId,
-            message,
-            regDate: lib.getDate(),
-        });
-        io.to(roomName).emit('chat message', chatMessage);
+    socket.on('chat message', async (meetingId, userId, message) => {
+        if (!meetingId || !userId) return;
+        try {
+            console.log(
+                'meetingId: ',
+                meetingId,
+                ' userId : ',
+                userId,
+                ' message : ',
+                message,
+            );
+            const existMember = await MEETINGMEMBER.findOne({
+                meetingId,
+                meetingMemberId: userId,
+            });
+            if (existMember) {
+                const userProfile = await USER.findOne({ userId });
+                const chatMessage = {
+                    userId,
+                    username: userProfile.username,
+                    profileImage: userProfile.profileImage,
+                    message,
+                    regDate: lib.getDate(),
+                };
+                io.to('meeting', meetingId).emit('chat message', chatMessage);
+                await CHAT.create({
+                    meetingId,
+                    userId,
+                    message,
+                    regDate: lib.getDate(),
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
     });
 });
 
