6e87307a8c810722c90eb8e03c94342a482cba38
MoingXTwice <sckbs1314@gmail.com>
2022-05-28 06:22:22 +0900

    chore(chat): delete chat API
    사용하지 않는 파일을 삭제하였습니다.
    
    2022.05.28 실전프로젝트 Book-ing
    commit by MoingXTwice

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const studyRouter = require('./study');
 const meetingRouter = require('./meeting');
 const mypage = require('./mypage');
 const search = require('./search');
-const chat = require('./chat');
 
 router.use('/study', studyRouter);
 router.use('/auth', kakao);
@@ -14,6 +13,5 @@ router.use('/main', mainRouter);
 router.use('/meeting', meetingRouter);
 router.use('/mypage', mypage);
 router.use('/search', search);
-router.use('/chat', chat);
 
 module.exports = router;
